63d958da5c752a6cb701b70c5c7e6e5935c21bb7
ALevan747 <ALevan747@sudomail.com>
2022-01-08 17:49:19 -0800

    Project 0 update 2

---
+++ trms-back/src/test/java/com/revature/data/DepartmentDAOTest.java
@@ -0,0 +1,44 @@
+package com.revature.data;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.*;
+
+import com.revature.utils.DAOFactory;
+
+public class DepartmentDAOTest {
+	private DepartmentDAO depDao = DAOFactory.getDepartmentDAO();
+	
+	@Test
+	public void getByIdNotNull()
+	{
+		assertNotNull(depDao.getById(1));
+	}
+	
+	@Test
+	public void getByIdNull()
+	{
+		assertNull(depDao.getById(2454));
+	}
+	
+	@Test
+	public void getAllNotNull()
+	{
+		assertNotNull(depDao.getAll());
+	}
+	
+	@Test
+	public void getByNameNotNull()
+	{
+		assertNotNull(depDao.getByName("Engineers"));
+	}
+	
+	
+	@Test
+	public void getByNameNull()
+	{
+		assertTrue(depDao.getByName("pokemon").isEmpty());
+	}
+	
+
+}
